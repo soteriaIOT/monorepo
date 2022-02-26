823124ffa6cc395584cd1c8e26f7cf1ba10fac6f
rkabani19 <rohail.kabani@gmail.com>
2022-02-26 13:10:41 -0500

    misc change to make work

--- application-server/server.go
+++ application-server/server.go
@@ -8,11 +8,21 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/arora-aditya/monorepo/application-server/graph/generated"
+	"github.com/go-chi/chi/v5"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -20,9 +30,9 @@ func main() {
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: NewMockResolver()}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
