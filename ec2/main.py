f1015b9b4a7e95fba1f61aed4d183cf1719824b5
Gregory Ho <huskit8520g@gmail.com>
2022-03-01 23:01:15 -0500

    Update metrics

--- ec2/main.py
+++ ec2/main.py
@@ -2,6 +2,7 @@ import time
 import datetime
 import numpy as np
 import math
+import random
 
 from dotenv import dotenv_values
 from InfluxHelper import InfluxHelper
@@ -11,7 +12,7 @@ INFLUX_USER = config.get("influx_username")
 INFLUX_PW = config.get("influx_password")
 INFLUX_IP = config.get("influx_ip")
 
-ONE_MINUTE = 60
+ONE_MINUTE = 10
 
 influx_helper = InfluxHelper(
     ip=INFLUX_IP, port=8086, username=INFLUX_USER, password=INFLUX_PW, db="test"
@@ -30,96 +31,120 @@ devices = [
 	"name": "raspi001",
 	"location": "E7-4235",
 	"type": "Raspberry Pi",
+    "device_cpu": 5.3,
+    "device_mem": 4.1,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,2),
 	"device_disk" : np.random.randint(5, 89)
 },
 {
 	"name": "raspi002",
 	"location": "E5-1445",
 	"type": "Raspberry Pi",
+    "device_cpu": 35.3,
+    "device_mem": 9.1,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,3),
 	"device_disk" : np.random.randint(5, 89)
 },
 {
 	"name": "raspi003",
 	"location": "E7-6125",
 	"type": "Raspberry Pi",
+    "device_cpu": 8.6,
+    "device_mem": 7.1,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,2),
 	"device_disk" : np.random.randint(5, 89)
 },
 {
 	"name": "raspi004",
 	"location": "E6-4012",
 	"type": "Raspberry Pi",
+    "device_cpu": 23.5,
+    "device_mem": 42.3,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,2),
 	"device_disk" : np.random.randint(5, 89)
 },
 {
 	"name": "raspi005",
 	"location": "E2-3445",
 	"type": "Raspberry Pi",
+    "device_cpu": 68.2,
+    "device_mem": 78.9,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,2),
 	"device_disk" : np.random.randint(5, 89)
 },
 {
 	"name": "raspi006",
 	"location": "E2-2134",
 	"type": "Raspberry Pi",
+    "device_cpu": 43.2,
+    "device_mem": 86.2,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,3),
 	"device_disk" : np.random.randint(5, 89)
 },
 {
 	"name": "raspi007",
 	"location": "E5-3415",
 	"type": "Raspberry Pi",
+    "device_cpu": 7.3,
+    "device_mem": 12.1,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,2),
 	"device_disk" : np.random.randint(5, 89)
 },
 {
 	"name": "ecserv1",
 	"location": "DC-1055",
 	"type": "Linux Server",
+    "device_cpu": 8.3,
+    "device_mem": 23.1,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,2),
 	"device_disk" : np.random.randint(5, 89)
 },
 {
 	"name": "ecserv2",
 	"location": "DC-1056",
 	"type": "Linux Server",
+    "device_cpu": 23.5,
+    "device_mem": 46.2,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,1),
 	"device_disk" : np.random.randint(5, 89)
 },
 {
 	"name": "ecserv3",
 	"location": "QNC-2315",
 	"type": "QDOT Server",
+    "device_cpu": 9.8,
+    "device_mem": 10.5,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,2),
 	"device_disk" : np.random.randint(5, 89)
 },
 {
 	"name": "FPGA-21",
 	"location": "E7-4261",
 	"type": "FPGA",
+    "device_cpu": 56.1,
+    "device_mem": 20.5,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,3),
 	"device_disk" : np.random.randint(5, 89)
 },
 {
 	"name": "FPGA-13",
 	"location": "E7-4268",
 	"type": "FPGA",
+    "device_cpu": 8.4,
+    "device_mem": 28.2,
 	"device_power": np.random.randint(10, 78),
-	"device_vulnerabilities": np.random.randint(0,6),
+	"device_vulnerabilities": np.random.randint(0,2),
 	"device_disk" : np.random.randint(5, 89)
 }
 ]
@@ -147,19 +172,18 @@ def get_active_vulnerabilities(active_vulnerabilities):
 		active_vulnerabilities = max(0, active_vulnerabilities + np.random.randint(-2, 2))
 	return active_vulnerabilities
 
-def add_device_metrics(name, location, type, device_power, device_vulnerabilities, device_disk):
-	
+def add_device_metrics(device):
     fields = {
-        "device_cpu": float(50 + np.random.randint(-40,40)),
-		"device_mem": float(5 + np.random.randint(-3,3)),
-		"device_power": device_power,
-		"device_vulnerabilities": device_vulnerabilities,
-		"device_disk": device_disk
+        "device_cpu": float(device["device_cpu"] + 5*random.random()),
+		"device_mem": float(device["device_mem"] + 10*random.random()),
+		"device_power": int(device["device_power"] + 10*random.random()),
+		"device_vulnerabilities": int(device["device_vulnerabilities"]),
+		"device_disk": int(device["device_disk"] + 10*random.random()),
     }
     tags = {
-        "device_type": type,
-        "host": name,
-        "location": location
+        "device_type": device["type"],
+        "host": device["name"],
+        "location": device["location"]
     }
     influx_helper.add_metric(name="mock", fields=fields, tags=tags)
 
@@ -169,7 +193,7 @@ def send_metrics(devices_old, vulnerabilities_old):
 	x = get_current_hour_seconds()
 	
 	fields = {
-		"cpu_avg": float(max(0, min(100, daily_sin(x) + random_noise())))
+		"cpu_avg": float(max(3, min(100, daily_sin(x+300) + random_noise())))
 	}
 	tags = {
         "device_type": "server",
@@ -179,7 +203,7 @@ def send_metrics(devices_old, vulnerabilities_old):
 	influx_helper.add_metric(name="mock", fields=fields, tags=tags)
 	
 	fields = {
-		"mem_avg": float(min(1.5*daily_sin(x) + random_noise(),98))
+		"mem_avg": float(max(3, min(1.5*daily_sin(x) + random_noise(),98)))
 	}
 	tags = {
 		"device_type": "server",
@@ -201,14 +225,7 @@ def send_metrics(devices_old, vulnerabilities_old):
 	influx_helper.add_metric(name="mock", fields=fields, tags={})
 	
 	for device in devices:
-		add_device_metrics(
-			device["name"],
-			device["location"],
-			device["type"],
-			device["device_power"],
-			device["device_vulnerabilities"],
-			device["device_disk"]
-		)
+		add_device_metrics(device)
 	
 	influx_helper.send()
 	return (devices_new, vulnerabilities_new)
